449f6ee1690a82ecf65f9f0b0f4f1e88fad57b00
Sean McDaniel <sean.mcdaniel@me.com>
2010-09-27 14:07:56 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Connect CORS
+
+## Installation
+
+    $ npm install jsonp-cors
+
+## Examples
+
+
+     var connect = require('connect'),
+         jsonp = require('connect-jsonp');
+     
+     var server = Connect.createServer(
+	    connect.bodyDecoder(),
+	    jsonp(),
+        function(req, res) {
+             res.writeHead(200, {'Content-Type': 'text/plain'});
+             res.end("{a: 'json string'");
+        }
+     );
+
+     server.listen(3000);
+
+
+## Testing
+
+    git submodule update --init
+    make test
+
+## TODO
+
+ - Make jsonp independent of connect's bodyDecode middleware.
+
+## License 
+
+(The MIT License)
+
+Copyright (c) 2010 Sean McDaniel
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ test/connect-jsonp.test.js
@@ -0,0 +1,99 @@
+var connect = require('connect'),
+    jsonp = require('connect-jsonp'),
+    helpers = require('./helpers'),
+    assert = require('assert'),
+    http = require('http');
+
+function server(headers, code) {
+    headers = headers || {};
+    code = code || 200;
+    return helpers.run(
+        connect.bodyDecoder(),
+        connect.gzip(),
+        jsonp(),
+        connect.createServer(
+            function(req, res) {
+                res.writeHead(code, headers);
+                res.write("{data: 'data'}")
+                res.end();
+            }
+        )
+    );
+}
+
+function assertPadded(res, padded) {
+	if (padded) {
+        assert.eql("cb({data: 'data'})", res.body);
+        assert.eql('application/javascript', res.headers['content-type']);
+	
+	} else {
+        assert.eql("{data: 'data'}", res.body);		
+	}
+}
+
+module.exports = {
+    'test not a padding request': function() {
+        var req = server().request('GET', '/', { });
+        req.buffer = true;
+        req.addListener('response', function(res) {
+            res.addListener('end', function() {
+                assertPadded(res, false);
+            });
+        });
+        req.end();
+    },
+
+    'test form request': function() {
+        var req = server().request('POST', '/', {'Content-Type': 'application/x-www-form-urlencoded'});
+        req.buffer = true;
+        req.addListener('response', function(res) {
+            res.addListener('end', function() {
+	            assertPadded(res, true);
+            });
+        });
+        req.write('callback=cb');
+        req.end();
+    },
+
+    'test json request': function() { 
+        var req = server().request('POST', '/', {'Content-Type': 'application/json; charset=utf8'});
+        req.buffer = true;
+        req.addListener('response', function(res) {
+            res.addListener('end', function(){
+	            assertPadded(res, true);
+            });
+        });
+        req.write('{"callback": "cb"}');
+        req.end();
+    },
+
+    'test error status code': function() { 
+        var req = server({}, 404).request('POST', '/', {'Content-Type': 'application/json; charset=utf8'});
+        req.buffer = true;
+        req.addListener('response', function(res) {
+            res.addListener('end', function() {
+	            assertPadded(res, false);
+            });
+        });
+        req.write('{"callback": "cb"}');
+        req.end();
+    },
+
+    'test plays nice with others': function() {
+        var req = server().request('POST', '/', {
+	        'Content-Type': 'application/x-www-form-urlencoded',
+         	'Accept-Encoding': 'deflate, gzip'
+	    });
+        req.buffer = true;
+        req.addListener('response', function(res) {
+            res.addListener('end', function() {
+	            assert.eql('gzip', res.headers['content-encoding']);
+		        assert.eql('application/javascript', res.headers['content-type']);
+		        // test compression
+  		        //assert.eql("cb({data: 'data'})", res.body);
+            });
+        });
+        req.write('callback=cb');
+        req.end();
+    }
+}
