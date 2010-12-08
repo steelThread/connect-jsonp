9328b8bdebf3e90ebc5f1784259fa54e56f913ad
Sean McDaniel <sean.mcdaniel@me.com>
2010-12-07 23:18:57 -0500

    Adding support for filtering the callback querystring param

--- test/connect-jsonp.test.js
+++ test/connect-jsonp.test.js
@@ -15,7 +15,7 @@ function server(headers, code) {
     code = code || 200;
     return helpers.run(
         connect.gzip(),
-        connect.jsonp(),
+        connect.jsonp(true),
         connect.createServer(
             function(req, res) {
                 res.writeHead(code, headers);
@@ -65,7 +65,7 @@ module.exports = {
     },
 
     'test query string': function() {
-        var req = server().request('GET', '/test?callback=cb', { });
+        var req = server().request('GET', '/test?callback=cb&test=test', { });
         req.buffer = true;
         req.addListener('response', function(res) {
             res.addListener('end', function() {
@@ -75,6 +75,16 @@ module.exports = {
         req.end();  
     },
 
+    'test query string removing callback param': function() {
+        var req = server().request('GET', '/test?callback=cb&test=test', { });
+        req.buffer = true;
+        req.addListener('response', function(res) {
+            res.addListener('end', function() {
+                assertPadded(res, true);
+            });
+        });
+        req.end();  
+    },
 
    'test plays nice with others': function() {
         var req = server().request('GET', '/test?callback=cb', {
